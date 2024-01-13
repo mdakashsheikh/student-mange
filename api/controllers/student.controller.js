a0d43ac34002640edf2b740a0b683fb93729cee6
MdAkashSheikh <nmakashsheikh@gmail.com>
2024-01-13 18:01:50 +0600

    add Home

--- api/controllers/student.controller.js
+++ api/controllers/student.controller.js
@@ -1,3 +1,4 @@
+const db = require('../db/dbConn.js');
 
 const postStudent = async(req, res) => {
 
@@ -9,6 +10,11 @@ const editStudent = async(req, res) => {
 
 const getStudent = async(req, res) => {
 
+    const sql = "SELECT * FROM student_info";
+    db.query(sql, (err, result) => {
+        if(err) return res.json({Message: 'Server Error'});
+        return res.json(result);
+    })
 }
 
 const deleteStudent = async(req, res) => {
